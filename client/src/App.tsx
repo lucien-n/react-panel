cd0e22fd680d3082999a951896bde591ddfd3e69
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-07-26 20:12:50 +0200

    session2: table display, icons, change client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,43 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Device } from "./types/device";
 import DeviceComponent from "./components/Device";
+import './App.css';
 
 const App = () => {
   const [backendData, setBackendData] = useState<Device[]>([]);
-
-  const clientId = 'flash';
+  const [clientId, setClientId] = useState<'zoom' | 'flash'>('flash');
 
   useEffect(() => {
     fetch(`/devices/${clientId}`)
       .then((response) => response.json())
       .then((data) => setBackendData(data));
-  }, []);
+  }, [clientId]);
 
-  console.log(backendData);
+  const handleSelectChange = (event: React.FormEvent) => {
+    const value = (event.target as any).value;
+    setClientId(value);
+  };
 
   return (
-    <div>
-      {backendData ? (
-        backendData.map((device: Device, i: number) => (
-          <DeviceComponent key={i} device={device}></DeviceComponent>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <section>
+      <select id="client" onChange={handleSelectChange}>
+        <option value="flash">Flash</option>
+        <option value="zoom">Zoom</option>
+      </select>
+      <table id="devices">
+        <tr>
+          <th>Serial Number</th>
+          <th>Security Status</th>
+        </tr>
+        {backendData ? (
+          backendData.map((device: Device, i: number) => (
+            <DeviceComponent key={i} device={device}></DeviceComponent>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </table>
+    </section>
   );
 };
 
--- client/src/components/Device.tsx
+++ client/src/components/Device.tsx
@@ -1,10 +1,20 @@
 import { Device } from "../types/device";
+import { Icon } from '@iconify/react';
 
-const DeviceComponent = ({device}: {device: Device}) => {
+const DeviceComponent = ({ device }: { device: Device; }) => {
 
-    return <>
-        <p>{device.serialNumber}</p>
-    </>
-}
+    return <tr>
+        <td>
+            {device.serialNumber}
+        </td>
+        <td>
+            <span>
+                {!device.security.antivirus ? (<Icon icon="mdi:antivirus" width="24px" color="red" />) : null}
+                {!device.security.encryption ? (<Icon icon="mdi:lock-off" width="24px" color="red" />) : null}
+                {!device.security.firewall ? (<Icon icon="mdi:wall" width="24px" color="red" />) : null}
+            </span>
+        </td>
+    </tr>;
+};
 
-export default DeviceComponent
+export default DeviceComponent;
