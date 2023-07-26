b995cf92873e9678d4927dc2c51bc232cb4828fe
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-07-26 22:49:38 +0200

    feat: silent after 30 days clock icon

--- client/src/components/Device.tsx
+++ client/src/components/Device.tsx
@@ -2,6 +2,11 @@ import { Device } from "../types/device";
 import { Icon } from '@iconify/react';
 
 const DeviceComponent = ({ device }: { device: Device; }) => {
+    const millisecondsInThirtyDays = 30 * 24 * 60 * 60 * 1_000;
+    const isDeviceSilent = (new Date().getTime() - new Date(device.lastCheckInDate * 1_000).getTime()) > millisecondsInThirtyDays;
+    const isFullySecured = Object.values(device.security).every((security) => security === true);
+
+    console.log(new Date().getTime() / 1000);
 
     return <tr>
         <td>
@@ -9,9 +14,19 @@ const DeviceComponent = ({ device }: { device: Device; }) => {
         </td>
         <td>
             <span>
-                {!device.security.antivirus ? (<Icon icon="mdi:antivirus" width="24px" color="red" />) : null}
-                {!device.security.encryption ? (<Icon icon="mdi:lock-off" width="24px" color="red" />) : null}
-                {!device.security.firewall ? (<Icon icon="mdi:wall" width="24px" color="red" />) : null}
+                {
+                    isDeviceSilent ? (<Icon icon="mdi:clock" width="24px" color="gray" />) :
+                        <>
+                            {isFullySecured ? (<Icon icon="mdi:shield-check" width="24px" color="green" />) :
+                                <>
+                                    {!device.security.antivirus ? (<Icon icon="mdi:antivirus" width="24px" color="red" />) : null}
+                                    {!device.security.encryption ? (<Icon icon="mdi:lock-off" width="24px" color="red" />) : null}
+                                    {!device.security.firewall ? (<Icon icon="mdi:wall" width="24px" color="red" />) : null}
+                                </>
+                            }
+                        </>
+                }
+
             </span>
         </td>
     </tr>;
